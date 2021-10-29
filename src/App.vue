4dca4ef38eb142a388fd6f53091eced7c9f57f7a
javierpb275 <javierpb275@gmail.com>
2021-10-29 18:13:20 +0200

    created User component and passed prop age to User from the App component.

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,24 @@
 <template>
   <div>
-    <p>hey!</p>
+    <h3>hey!</h3>
     <greeting></greeting>
+    <user :age="age"></user>
   </div>
 </template>
 
 <script>
 import Greeting from "./components/Greeting.vue";
+import User from "./components/User.vue";
 export default {
   name: "App",
   components: {
     Greeting,
+    User,
+  },
+  data() {
+    return {
+      age: 20,
+    };
   },
 };
 </script>
