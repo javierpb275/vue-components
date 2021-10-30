02e08768a6862c32290b24b203a118caf4f36421
javierpb275 <javierpb275@gmail.com>
2021-10-30 18:16:25 +0200

    specified prop age with a type and other properties.

--- src/components/User.vue
+++ src/components/User.vue
@@ -2,14 +2,23 @@
   <div>
     <button type="button" @click="onClickAge">Update Age</button>
     <p>The user is {{ age }} years old</p>
-    <p>{{ageDoubled}}</p>
+    <p>{{ ageDoubled }}</p>
   </div>
 </template>
 
 <script>
 export default {
   name: "User",
-  props: ["age"],
+  props: {
+    age: {
+      type: Number,
+      required: true,
+      default: 20,
+      validator(value) {
+        return value < 130;
+      },
+    },
+  },
   emits: ["age-change"],
   computed: {
     ageDoubled() {
